00b6b027df0a40cfd8ca88f8feea9c8c46db4359
sung-il <j.sungil099@gmail.com>
2020-06-09 12:45:24 +0900

    edit test.py

--- test.py
+++ test.py
@@ -1,22 +1,6 @@
 import asyncio
 
-<<<<<<< HEAD
-# import yaml
-# try:
-#     from yaml import CLoader as Loader, CDumper as Dumper
-# except ImportError:
-#     from yaml import Loader, Dumper
-=======
-import yaml
-try:
-    from yaml import CLoader as Loader, CDumper as Dumper
-except ImportError:
-    from yaml import Loader, Dumper
->>>>>>> abe828df8b16335f968db098f3b528813f682665
-
 from hfc.fabric import Client
-
-
 
 #print(hfc.VERSION)
 
@@ -29,17 +13,16 @@ from hfc.fabric import Client
 
 
 loop = asyncio.get_event_loop()
-<<<<<<< HEAD
-cli = Client(net_profile="first-network.json")
-org1_admin = cli.get_user('Org1MSP', 'Admin')
+# cli = Client(net_profile="first-network.json")
+# org1_admin = cli.get_user('Org1MSP', 'Admin')
 
-response = loop.run_until_complete(cli.query_installed_chaincodes(
-               requestor=org1_admin,
+# response = loop.run_until_complete(cli.query_installed_chaincodes(
+#                requestor=org1_admin,
                
-               peers=['peer0.org1.example.com'],
-               decode=True
-               ))
-=======
+#                peers=['peer0.org1.example.com'],
+#                decode=True
+#                ))
+
 cli = Client(net_profile="test-network.json")
 org1_admin = cli.get_user('org1.example.com', 'Admin')
 
@@ -48,7 +31,6 @@ org1_admin = cli.get_user('org1.example.com', 'Admin')
 #                peers=['peer0.org1.example.com'],
 #                decode=True
 #                ))
->>>>>>> abe828df8b16335f968db098f3b528813f682665
 
 
 
@@ -58,9 +40,6 @@ org1_admin = cli.get_user('org1.example.com', 'Admin')
 #                decode=True
 #                ))
 
-<<<<<<< HEAD
-# print(response)
-=======
 # print(response)
 
 
@@ -76,4 +55,3 @@ response = loop.run_until_complete(cli.chaincode_query(
                cc_name='mycc'
                ))
 print(response)
->>>>>>> abe828df8b16335f968db098f3b528813f682665
