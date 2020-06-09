abe828df8b16335f968db098f3b528813f682665
sung-il <j.sungil099@gmail.com>
2020-06-09 11:55:34 +0900

    update test-net

--- test.py
+++ test.py
@@ -21,8 +21,8 @@ from hfc.fabric import Client
 
 
 loop = asyncio.get_event_loop()
-cli = Client(net_profile="first-network.json")
-org1_admin = cli.get_user('Org1MSP', 'Admin')
+cli = Client(net_profile="test-network.json")
+org1_admin = cli.get_user('org1.example.com', 'Admin')
 
 # response = loop.run_until_complete(cli.query_installed_chaincodes(
 #                requestor=org1_admin,
@@ -32,10 +32,24 @@ org1_admin = cli.get_user('Org1MSP', 'Admin')
 
 
 
-response = loop.run_until_complete(cli.query_channels(
-               requestor=org1_admin,
-               peers=['peer0.org1.example.com'],
-               decode=True
-               ))
+# response = loop.run_until_complete(cli.query_channels(
+#                requestor=org1_admin,
+#                peers=['peer0.org1.example.com'],
+#                decode=True
+#                ))
 
+# print(response)
+
+
+
+# Query a chaincode
+args = ['a']
+# The response should be true if succeed
+response = loop.run_until_complete(cli.chaincode_query(
+               requestor=org1_admin,
+               channel_name='mychannel',
+               peers=['peer0.org1.example.com'],
+               args=args,
+               cc_name='mycc'
+               ))
 print(response)
