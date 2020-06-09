50b2e400d1d2934b457b867c19072ccee97c26a0
sung-il <j.sungil099@gmail.com>
2020-06-09 11:12:13 +0900

    update

---
+++ query.py
@@ -0,0 +1,19 @@
+import asyncio
+from hfc.fabric import Client
+
+loop = asyncio.get_event_loop()
+
+cli = Client(net_profile="first-network.json")
+org1_admin = cli.get_user('Org1MSP', 'Admin')
+
+# Query a chaincode
+args = ['a']
+# The response should be true if succeed
+response = loop.run_until_complete(cli.chaincode_query(
+               requestor=org1_admin,
+               channel_name='mychannel',
+               peers=['peer0.org1.example.com'],
+               args=args,
+               cc_name='mycc'
+               ))
+print(response)
