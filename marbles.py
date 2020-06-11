bd432d5bcc93c92a75f67e7ad506cddaafc53c6d
sung-il <j.sungil099@gmail.com>
2020-06-11 13:02:38 +0900

    update marbles log (data.log, marbles.py)

--- marbles.py
+++ marbles.py
@@ -9,10 +9,10 @@ loop = asyncio.get_event_loop()
 cli = Client(net_profile="test-network.json")
 org1_admin = cli.get_user('org1.example.com', 'Admin')
 
-name="TE"
+name="REE"
 color="sky"
 size="35"
-owner="TE"
+owner="REE"
 
 # initMarbles
 # for i in range(10,51):
@@ -43,47 +43,47 @@ owner="TE"
 #             ))
 # print(response)
 
-# for i in range(10000):
-#     args = [name+str(i), color, size, owner+str(i)]
-#     response = loop.run_until_complete(cli.chaincode_invoke(
-#             requestor=org1_admin,
-#             channel_name='mychannel',
-#             peers=['peer0.org1.example.com'],
-#             args=args,
-#             fcn='initMarble',
-#             cc_name='mymarbles',
-#             wait_for_event=True # for being sure chaincode invocation has been commited in the ledger, default is on tx event
-            # ))
-    # print("initMarble")
+for i in range(20):
+    args = [name+str(i), color, size, owner+str(i)]
+    response = loop.run_until_complete(cli.chaincode_invoke(
+            requestor=org1_admin,
+            channel_name='mychannel',
+            peers=['peer0.org1.example.com'],
+            args=args,
+            fcn='initMarble',
+            cc_name='mymarbles',
+            wait_for_event=True # for being sure chaincode invocation has been commited in the ledger, default is on tx event
+            ))
+    print("initMarble")
     # print(response)
 
-# for i in range(1):
-#     args = ["doc02"]
-#     response = loop.run_until_complete(cli.chaincode_invoke(
-#             requestor=org1_admin,
-#             channel_name='mychannel',
-#             peers=['peer0.org1.example.com'],
-#             args=args,
-#             fcn='readMarble',
-#             cc_name='mymarbles',
-#             wait_for_event=True # for being sure chaincode invocation has been commited in the ledger, default is on tx event
-#             ))
-#     print("readMarble")
+for i in range(20):
+    args = [name+str(i)]
+    response = loop.run_until_complete(cli.chaincode_invoke(
+            requestor=org1_admin,
+            channel_name='mychannel',
+            peers=['peer0.org1.example.com'],
+            args=args,
+            fcn='readMarble',
+            cc_name='mymarbles',
+            wait_for_event=True # for being sure chaincode invocation has been commited in the ledger, default is on tx event
+            ))
+    print("readMarble")
 #     print(response)
 
-# for i in range(1):
-#     args = ["0"]
-#     response = loop.run_until_complete(cli.chaincode_invoke(
-#             requestor=org1_admin,
-#             channel_name='mychannel',
-#             peers=['peer0.org1.example.com'],
-#             args=args,
-#             fcn='getHistoryForMarble',
-#             cc_name='mymarbles',
-#             wait_for_event=True # for being sure chaincode invocation has been commited in the ledger, default is on tx event
-#             ))
-#     print("getHistoryForMarble")
-#     print(response)
+for i in range(20):
+    args = ["0"]
+    response = loop.run_until_complete(cli.chaincode_invoke(
+            requestor=org1_admin,
+            channel_name='mychannel',
+            peers=['peer0.org1.example.com'],
+            args=args,
+            fcn='getHistoryForMarble',
+            cc_name='mymarbles',
+            wait_for_event=True # for being sure chaincode invocation has been commited in the ledger, default is on tx event
+            ))
+    print("getHistoryForMarble")
+    # print(response)
 
 
 
@@ -102,15 +102,30 @@ owner="TE"
     #print("getMarblesByRange")
     #print(response)
 
-# for i in range(20):
-#     args = ["RT0", "RT99"]
-#     response = loop.run_until_complete(cli.chaincode_invoke(
-#             requestor=org1_admin,
-#             channel_name='mychannel',
-#             peers=['peer0.org1.example.com'],
-#             args=args,
-#             fcn='getMarblesByRange',
-#             cc_name='mymarbles',
-#             wait_for_event=True # for being sure chaincode invocation has been commited in the ledger, default is on tx event
-#             ))
+for i in range(20):
+    args = ["RT0", "RT99"]
+    response = loop.run_until_complete(cli.chaincode_invoke(
+            requestor=org1_admin,
+            channel_name='mychannel',
+            peers=['peer0.org1.example.com'],
+            args=args,
+            fcn='getMarblesByRange',
+            cc_name='mymarbles',
+            wait_for_event=True # for being sure chaincode invocation has been commited in the ledger, default is on tx event
+            ))
+    print("0~99")
+    # print(response)
+
+for i in range(20):
+    args = ["RT0", "RT9999"]
+    response = loop.run_until_complete(cli.chaincode_invoke(
+            requestor=org1_admin,
+            channel_name='mychannel',
+            peers=['peer0.org1.example.com'],
+            args=args,
+            fcn='getMarblesByRange',
+            cc_name='mymarbles',
+            wait_for_event=True # for being sure chaincode invocation has been commited in the ledger, default is on tx event
+            ))
+    print("0~9999")
     # print(response)
